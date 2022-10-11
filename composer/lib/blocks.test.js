e8c5d7d2fa4aeadc465323f55ebdc222c1e81fb8
deepak <anandhadeepak@gmail.com>
2022-10-11 13:23:28 +0530

    removed composer module

--- composer/lib/blocks.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react'
-import Blocks from './blocks'
-
-test('given a list of domain block, they should be rendered', () => {
-  render(<Blocks domainBlocks="123"/>)
-  screen.getByText(/123/i)
-  expect(1).toBe(1)
-})
