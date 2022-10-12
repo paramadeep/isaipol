dd6e6af1eb17f51a8fbeff8906059cee1c1a265a
deepak <anandhadeepak@gmail.com>
2022-10-12 15:01:01 +0530

    refactor test

--- web/src/components/Blocks.test.js
+++ web/src/components/Blocks.test.js
@@ -1,20 +1,24 @@
 import { render, screen, within } from "@testing-library/react";
 import Blocks from "./Blocks";
 
+jest.mock("./BlockInput", () => {
+  return { BlockInput: ({ input }) => <div>{`BlockInput${input}`}</div> };
+});
+
 describe("Blocks", () => {
   test("should render all blocks", () => {
     let blocks = [{ name: "quantity" }, { name: "dimension" }];
     render(<Blocks blocks={blocks} />);
-    expect(screen.getByTestId("quantityBlock")).not.toBeNull();
+    expect(screen.getByTestId("quantityBlock")).toBeVisible();
     expect(screen.getByTestId("quantityBlock")).toHaveTextContent("quantity");
     expect(screen.getByTestId("dimensionBlock")).toHaveTextContent("dimension");
-    expect(screen.getByTestId("dimensionBlock")).not.toBeNull();
+    expect(screen.getByTestId("dimensionBlock")).toBeVisible();
   });
 
   test("should render input fields", () => {
     render(<Blocks blocks={[{ name: "q", input: 10 }]} />);
     expect(
-      within(screen.getByTestId("qBlock")).getByDisplayValue("10")
-    ).not.toBeNull();
+      within(screen.getByTestId("qBlock")).getByText("BlockInput10")
+    ).toBeVisible();
   });
 });
