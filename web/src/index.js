326df857fc7661c41e87b5c816abde56b49c6185
deepak <anandhadeepak@gmail.com>
2022-10-19 16:30:27 +0530

    add debug atom

--- web/src/index.js
+++ web/src/index.js
@@ -3,11 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import DebugAtom from "./states/DebugAtom";
+import { Provider } from "jotai";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <DebugAtom />
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
